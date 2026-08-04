0f350cffe02bc931fe26cfc3e94011886f588e47
Antony <telesantony51@gmail.com>
2026-08-04 15:59:26 -0300

    inicial commit

---
+++ src/main/java/com/example/it_to_do/ItToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.it_to_do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/it_to_do/ItToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it_to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
